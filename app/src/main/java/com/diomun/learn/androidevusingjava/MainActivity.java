18469d29849120fe41bb69f775c11bf1080f245f
DiaoMengQ <dmq1212@qq.com>
2020-12-29 20:28:55 +0800

    Inital project
    Signed-off-by: DiaoMengQ <dmq1212@qq.com>

---
+++ app/src/main/java/com/diomun/learn/androidevusingjava/MainActivity.java
@@ -0,0 +1,13 @@
+package com.diomun.learn.androidevusingjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
