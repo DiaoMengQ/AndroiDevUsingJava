03a57e585708e0688d2ad00264ca317cffb577ba
DiaoMnegQ <dmq1212@qq.com>
2021-01-06 02:21:18 +0800

    adjust layout
    Signed-off-by: DiaoMnegQ <dmq1212@qq.com>

--- app/src/main/java/com/diomun/learn/androidevusingjava/activity/MainActivity.java
+++ app/src/main/java/com/diomun/learn/androidevusingjava/activity/MainActivity.java
@@ -3,16 +3,25 @@ package com.diomun.learn.androidevusingjava.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import com.diomun.learn.androidevusingjava.R;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
+    private Button toLVBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+    }
+
+//    初始化页面控件
+    private void initUI() {
+        toLVBtn = this.findViewById(R.id.btn_toListView);
     }
 
     @Override
