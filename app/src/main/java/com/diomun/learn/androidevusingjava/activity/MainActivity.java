617fc8bf0896e77a22c90bbb611e70d1721f7108
DiaoMnegQ <dmq1212@qq.com>
2021-01-06 02:40:22 +0800

    ButterKnife learning
    Signed-off-by: DiaoMnegQ <dmq1212@qq.com>

--- app/src/main/java/com/diomun/learn/androidevusingjava/activity/MainActivity.java
+++ app/src/main/java/com/diomun/learn/androidevusingjava/activity/MainActivity.java
@@ -1,65 +1,88 @@
 package com.diomun.learn.androidevusingjava.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.diomun.learn.androidevusingjava.BuildConfig;
 import com.diomun.learn.androidevusingjava.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @author DiaoMengQ
+ */
 public class MainActivity extends AppCompatActivity {
-    private String TAG = "MainActivity";
-    private Button toLVBtn;
+    private final String TAG = "MainActivity";
+
+    @BindView(R.id.tv_hello)
+    TextView tvHello;
+    @BindView(R.id.btn_toListView)
+    Button btnToListView;
+    private Button toLvBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        initUI();
-    }
-
-//    初始化页面控件
-    private void initUI() {
-        toLVBtn = this.findViewById(R.id.btn_toListView);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG,"onStart");
+        if (BuildConfig.DEBUG) {
+            Log.d("MainActivity", "testlog");
+        }
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,"onRestart");
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"onResume");
-
+        Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,"onPause");
-
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,"onStop");
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"onDestroy");
+        Log.i(TAG, "onDestroy");
     }
 
+    @OnClick({R.id.btn_toListView})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_toListView:
+                Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
+    }
 }
