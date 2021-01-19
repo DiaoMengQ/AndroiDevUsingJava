d7a1331d8a0232a684b46c4d2c4af45f23e0f3ae
DiaoMnegQ <dmq1212@qq.com>
2021-01-20 00:56:19 +0800

    add BaseActivity
    Signed-off-by: DiaoMnegQ <dmq1212@qq.com>

---
+++ app/src/main/java/com/diomun/learn/androidevusingjava/ui/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.diomun.learn.androidevusingjava.ui.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author: DiaoMengQi
+ * Email: dmq1212@qq.com
+ * created on 2021/1/19
+ */
+public class BaseActivity extends AppCompatActivity {
+    protected final String TAG = getClass().getSimpleName();
+    protected BaseActivity mContext;
+
+}
