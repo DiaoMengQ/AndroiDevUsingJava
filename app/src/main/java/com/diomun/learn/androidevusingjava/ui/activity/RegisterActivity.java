e5c7f5f3bea46f4bd94a8719bb367e26a305a44d
DiaoMnegQ <dmq1212@qq.com>
2021-01-23 22:30:45 +0800

    add register page & activity
    Signed-off-by: DiaoMnegQ <dmq1212@qq.com>

---
+++ app/src/main/java/com/diomun/learn/androidevusingjava/ui/activity/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.diomun.learn.androidevusingjava.ui.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
+import com.diomun.learn.androidevusingjava.R;
+import com.diomun.learn.androidevusingjava.base.BaseActivity;
+
+/**
+ * @author DIOMUN dmq1212@qq.com
+ * @date created on 2021/1/23
+ */
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    public int initLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
