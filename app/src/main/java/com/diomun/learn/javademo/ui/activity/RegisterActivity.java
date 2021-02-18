6cdbfb9a32b3ba015faebf4870fa99033359e648
DiaoMengQ <dmq1212@qq.com>
2021-02-18 15:22:56 +0800

    调整LinerLayout布局权重；显示头像框
    Signed-off-by: DiaoMengQ <dmq1212@qq.com>

--- app/src/main/java/com/diomun/learn/javademo/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/diomun/learn/javademo/ui/activity/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.diomun.learn.javademo.ui.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
