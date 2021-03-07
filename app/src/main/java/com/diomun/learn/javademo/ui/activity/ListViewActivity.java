926c0524c2afaf1e78555a97a512810638f34f77
DiaoMengQ <dmq1212@qq.com>
2021-03-07 20:27:19 +0800

    增加 MyListAdapter自定义Adapter基本类的使用
    Signed-off-by: DiaoMengQ <dmq1212@qq.com>

--- app/src/main/java/com/diomun/learn/javademo/ui/activity/ListViewActivity.java
+++ app/src/main/java/com/diomun/learn/javademo/ui/activity/ListViewActivity.java
@@ -1,11 +1,10 @@
 package com.diomun.learn.javademo.ui.activity;
 
-import android.util.Log;
 import android.widget.ListView;
 
 import com.diomun.learn.javademo.R;
-import com.diomun.learn.javademo.adapter.SongAdapter;
 import com.diomun.learn.javademo.base.BaseActivity;
+import com.diomun.learn.javademo.base.MyListAdapter;
 import com.diomun.learn.javademo.model.Song;
 
 import java.util.LinkedList;
@@ -23,7 +22,7 @@ public class ListViewActivity extends BaseActivity {
     @BindView(R.id.lv_song)
     ListView lvSong;
     private List<Song> songList = null;
-    private SongAdapter mAdapter = null;
+    private MyListAdapter<Song> mAdapter = null;
 
     @Override
     public int initLayout() {
@@ -34,17 +33,32 @@ public class ListViewActivity extends BaseActivity {
     public void initView() {
         ButterKnife.bind(this);
 
-        mAdapter = new SongAdapter((LinkedList<Song>) songList, mContext);
-        Log.d(TAG, "initView: " + songList.get(0).getsName());
+        // 原始Adapter方法
+        // SongAdapter mAdapter = new SongAdapter((LinkedList<Song>) songList, mContext);
+        // Log.d(TAG, "initView: " + songList.get(0).getsName());
+
         lvSong.setAdapter(mAdapter);
     }
 
     @Override
     public void initData() {
-        songList = new LinkedList<Song>();
+        songList = new LinkedList<>();
         for (int i = 0; i < 20; i++) {
             String str = String.valueOf(i);
             songList.add(new Song(str, str, str));
         }
+
+        // 使用自定义 MyBaseAdapter
+        mAdapter = new MyListAdapter<Song>((LinkedList<Song>) songList, R.layout.item_list_song) {
+
+            // 完成原本自定义Adapter类中的功能(SongAdapter)
+            @Override
+            protected void bindView(ViewHolder holder, Song obj) {
+                holder.setImageResource(R.id.imgV_songAlbum, R.mipmap.heiyu);
+                holder.setText(R.id.tv_songName, obj.getsName());
+                holder.setText(R.id.tv_songSinger, obj.getsSinger());
+            }
+        };
+
     }
 }
